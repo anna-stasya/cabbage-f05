9a25dfa1eac171c7f81e31086bad168b83459b84
Anastasiia Sanatar <sanatar.nastya@gmail.com>
2021-12-12 18:19:16 +0200

    draft version

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Container from './components/Container';
+//Auth
+import RegisterView from './redux/views/auth/RegisterView';
+import LoginViews from './redux/views/auth/LoginViews';
 
 import './App.css';
 
 const Example = lazy(() =>
   import('./pages/Example' /* webpackChunkName: "Example" */),
 );
+//Auth
+//const {LoginViews, RegisterView} = lazy(() => import('./redux/views/auth'));
+// const LoginViews = lazy(() => import('./redux/views/auth'));
+// const RegisterView = lazy(() => import('./redux/views/auth'));
 
 function App() {
   return (
     <Container>
       <Suspense fallback={<div>Downloading...</div>}>
+        
         <Routes>
+          <Route path="/register"  element={<RegisterView/>} />
+          <Route path="/login" exact element={<LoginViews/>}/> 
+            
           <Route end path="/" element={<Example />} />
         </Routes>
       </Suspense>
