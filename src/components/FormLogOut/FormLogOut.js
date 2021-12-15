362987d3fa1ba7a9857d91114ef7225b8db99c65
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2021-12-15 20:57:29 +0100

    update universal and logout form

--- src/components/FormLogOut/FormLogOut.js
+++ src/components/FormLogOut/FormLogOut.js
@@ -5,8 +5,10 @@ import s from './FormLogOut.module.css';
 
 export default function UniversalForm() {
   return (
-    <form className={s.form}>
-      <BiX className={s.iconClose} />
+    <form>
+      <div className={s.iconForm}>
+        <BiX className={s.iconClose} />
+      </div>
       <p className={s.question}>Вы действительно хотите выйти?</p>
       <div className={s.buttons}>
         <button
--- src/components/UniversalForm/UniversalForm.js
+++ src/components/UniversalForm/UniversalForm.js
@@ -5,8 +5,10 @@ import s from './UniversalForm.module.css';
 
 export default function UniversalForm() {
   return (
-    <form className={s.form}>
-      <BiX className={s.iconClose} />
+    <form>
+      <div className={s.iconForm}>
+        <BiX className={s.iconClose} />
+      </div>
       <p className={s.question}>Вы уверены?</p>
       <div className={s.buttons}>
         <button
