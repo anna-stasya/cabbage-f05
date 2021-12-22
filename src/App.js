7aa971e4ecb105f2734b5b3ad9e42ac3c0ea8ef0
Artur Kutusenko <kutusenko@gmail.com>
2021-12-22 19:35:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
-// import { Suspense, lazy } from 'react';
-// import { useSelector } from 'react-redux';
-// import { Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 
-// import { paths } from './config';
-//components
-// import Container from './components/Container';
-// import AppBar from './components/Header/appBar';
-// import UserMenu from './components/Header/userMenu';
-// import PrivateRoute from './components/Route/PrivateRoute';
-// import PublicRoute from './components/Route/PublicRoute';
-//Transactions
-// import TransactionView from './pages/Transactions/TransactionView';
+import { paths } from './config';
+// components;
+import Container from './components/Container';
+import AppBar from './components/Header/appBar';
+import UserMenu from './components/Header/userMenu';
+import PrivateRoute from './components/Route/PrivateRoute';
+import PublicRoute from './components/Route/PublicRoute';
+// Transactions;
+import TransactionView from './pages/Transactions/TransactionView';
 
-// import authSelectors from './redux/auth/auth-selectors';
+import authSelectors from './redux/auth/auth-selectors';
 import './App.css';
-// import styles from './components/Header/Header.module.css';
+import styles from './components/Header/Header.module.css';
 
-//Auth
-// const Login = lazy(() => import('./pages/Auth/Login/Login'));
-// const Registration = lazy(() =>
-//   import('./pages/Auth/Registration/Registration'),
-// );
-// const Reports = lazy(() =>
-//   import('./pages/Reports' /* webpackChunkName: "Reports" */),
-// );
-
-import ReportsCategories from './components/ReportsCategories';
+// Auth;
+const Login = lazy(() => import('./pages/Auth/Login/Login'));
+const Registration = lazy(() =>
+  import('./pages/Auth/Registration/Registration'),
+);
+const Reports = lazy(() =>
+  import('./pages/Reports' /* webpackChunkName: "Reports" */),
+);
 
 function App() {
-  // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   return (
     <>
-      <ReportsCategories />
-      {/* <header className={styles.headerContainer}>
+      <header className={styles.headerContainer}>
         <AppBar></AppBar>
         {isLoggedIn ? <UserMenu /> : null}
       </header>
-     <Container>
-      <Suspense fallback={<div>Downloading...</div>}>
-        <Routes>
-          <Route end path={paths.reports} element={<Reports />} />
-          <Route path={paths.register} exact element={<Registration />} />
-          <Route path={paths.login} exact element={<Login />} />
-          <Route path="/transactions" exact element={<TransactionView />} />
-        </Routes>
-      </Suspense>
-    </Container> */}
+      <Container>
+        <Suspense fallback={<div>Downloading...</div>}>
+          <Routes>
+            <Route end path={paths.reports} element={<Reports />} />
+            <Route path={paths.register} exact element={<Registration />} />
+            <Route path={paths.login} exact element={<Login />} />
+            <Route path="/transactions" exact element={<TransactionView />} />
+          </Routes>
+        </Suspense>
+      </Container>
     </>
   );
 
