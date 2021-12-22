6a348ec1bed193c9157e50b7450fef63515d83b2
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2021-12-22 19:43:29 +0100

    at work!!! :)

--- src/components/ModalUniversal/ModalUniversal.js
+++ src/components/ModalUniversal/ModalUniversal.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { BiX } from 'react-icons/bi';
+
+import getUsername from '../../redux/auth/auth-selectors';
+import logOut from '../../redux/auth/auth-operations';
 
 import s from './ModalUniversal.module.css';
 
 export default function ModalUniversal({ children, onClose }) {
   const [enterActive, setEnterActive] = useState(true);
   const [registerActive, setRegisterActive] = useState(false);
+  const dispatch = useDispatch();
+  const userName = useSelector(getUsername.getUsername);
 
   useEffect(() => {
     const icon = document.getElementById('icon-close');
     const btnNo = document.getElementById('btnNo');
+    const btnYes = document.getElementById('buttons');
 
     btnNo.addEventListener('click', onClose);
     icon.addEventListener('click', onClose);
     return () => {
       btnNo.removeEventListener('click', onClose);
+      btnYes.removeEventListener('click', onClose);
       icon.removeEventListener('click', onClose);
     };
   }, [onClose]);
 
   const toggleEnterActiveBtn = () => {
+    const btnYes = document.getElementById('buttons');
     setEnterActive(true);
     setRegisterActive(false);
+
+    dispatch(logOut(userName));
+    btnYes.addEventListener('click', onClose);
+    onClose();
   };
 
   const toggleRegisterActiveBtn = () => {
@@ -36,6 +50,7 @@ export default function ModalUniversal({ children, onClose }) {
         <button
           className={s.button}
           type="submit"
+          id="btnYes"
           active={enterActive}
           name="yes"
           onClick={toggleEnterActiveBtn}
