ad35e15a6deb94619d86a95ac58518e00f12a290
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2021-12-23 12:24:40 +0100

    убрал код для бэкграунда TransactionView

--- src/pages/Transactions/TransactionView.js
+++ src/pages/Transactions/TransactionView.js
@@ -4,29 +4,31 @@ import Balance from '../../components/Balance';
 import ToReportsBtn from '../../components/ToReportsBtn';
 import BriefList from '../../components/Brief';
 
-import HomeMain from '../../components/HomeMain/HomeMain';
-import Wrapper from '../../components/Wrapper/Wrapper';
-import s from './TransactionView.module.css';
+// import HomeMain from '../../components/HomeMain/HomeMain';
+// import Wrapper from '../../components/Wrapper/Wrapper';
+// import s from './TransactionView.module.css';
 
 export default function TransactionView() {
   return (
-    <div className={s.main_container}>
-      <HomeMain>
-        <Wrapper>
-          <div className={s.home_container}>
-            <div className={s.table_container}>
-              <ToReportsBtn />
-              <Tabs />
-            </div>
-          </div>
-          <div className={s.balance}>
-            <Balance />
-          </div>
-          <div className={s.reports}>
-            <BriefList />
-          </div>
-        </Wrapper>
-      </HomeMain>
+    <div
+    // className={s.main_container}
+    >
+      {/* <HomeMain> */}
+      {/* <Wrapper> */}
+      {/* <div className={s.home_container}> */}
+      {/* <div className={s.table_container}>. */}
+      <ToReportsBtn />
+      <Tabs />
+      {/* </div> */}
+      {/* </div> */}
+      {/* <div className={s.balance}> */}
+      <Balance />
+      {/* </div> */}
+      {/* <div className={s.reports}> */}
+      <BriefList />
+      {/* </div> */}
+      {/* </Wrapper> */}
+      {/* </HomeMain> */}
     </div>
   );
 }
